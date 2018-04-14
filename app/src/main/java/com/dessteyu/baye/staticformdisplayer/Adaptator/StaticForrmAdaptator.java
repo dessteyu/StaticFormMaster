abb93fe836b32915146ef2b7079dee6141db84f3
dessteyu <bayembye10@gmaail.com>
2018-04-14 17:25:17 +0000

    - add of the baseElements. - add begin of StaticFormAdaptator

--- app/src/main/java/com/dessteyu/baye/staticformdisplayer/Adaptator/StaticForrmAdaptator.java
+++ app/src/main/java/com/dessteyu/baye/staticformdisplayer/Adaptator/StaticForrmAdaptator.java
@@ -2,9 +2,12 @@ package com.dessteyu.baye.staticformdisplayer.Adaptator;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.dessteyu.baye.staticformdisplayer.R;
 import com.dessteyu.baye.staticformdisplayer.model.BaseStaticForm;
 
 import java.util.ArrayList;
@@ -26,23 +29,72 @@ public class StaticForrmAdaptator extends RecyclerView.Adapter<RecyclerView.View
 
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        RecyclerView.ViewHolder holder = null;
+        switch (viewType) {
+            case BaseStaticForm.HEADER:
+                holder = new HeaderViewHolder(LayoutInflater.from(context).inflate(R.layout.header_layout, null));
+            case BaseStaticForm.LABEL_AND_VALUE:
+                holder = new HeaderViewHolder(LayoutInflater.from(context).inflate(R.layout.label_and_value_layout, null));
+        }
+        return holder;
     }
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-
+        BaseStaticForm baseForm = baseStaticForms.get(position);
+        switch (baseForm.getTypeView()) {
+            case BaseStaticForm.HEADER:
+                HeaderViewHolder headerViewHolder = (HeaderViewHolder) holder;
+                headerViewHolder.headerElement.setText(baseForm.getValue() != null ? baseForm.getValue() : baseForm.getLabel());
+                break;
+            case BaseStaticForm.LABEL_AND_VALUE:
+                LabelAndValueViewHolder labelAndValueViewHolder = (LabelAndValueViewHolder) holder;
+                labelAndValueViewHolder.valueElement.setText(baseForm.getValue());
+                labelAndValueViewHolder.labelElement.setText(baseForm.getLabel());
+                break;
+        }
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return baseStaticForms.size();
     }
 
-    public class HeaderHolder extends RecyclerView.ViewHolder{
 
-        public HeaderHolder(View itemView) {
+    //    return the corresponding View
+
+
+    @Override
+    public int getItemViewType(int position) {
+        return baseStaticForms.get(position).getTypeView();
+    }
+
+
+    // this class is for displaying header
+//    TODO:: add listener
+    public class HeaderViewHolder extends RecyclerView.ViewHolder {
+        private TextView headerElement;
+
+        public HeaderViewHolder(View itemView) {
             super(itemView);
+            headerElement = (TextView) itemView.findViewById(R.id.header_element);
         }
+    }
+
+    //    this class is for displaying label and value text
+//    TODO:: add listener
+    public class LabelAndValueViewHolder extends RecyclerView.ViewHolder {
+        private TextView labelElement;
+        private TextView valueElement;
+
+        public LabelAndValueViewHolder(View itemView) {
+            super(itemView);
+            labelElement = (TextView) itemView.findViewById(R.id.label_element);
+            valueElement = (TextView) itemView.findViewById(R.id.value_element);
+        }
+    }
+
+    public void addElements(ArrayList<BaseStaticForm> baseStaticForms) {
+        this.baseStaticForms = baseStaticForms != null ? baseStaticForms : new ArrayList<BaseStaticForm>();
     }
 }
--- app/src/main/java/com/dessteyu/baye/staticformdisplayer/model/BaseStaticForm.java
+++ app/src/main/java/com/dessteyu/baye/staticformdisplayer/model/BaseStaticForm.java
@@ -5,4 +5,42 @@ package com.dessteyu.baye.staticformdisplayer.model;
  */
 
 public class BaseStaticForm {
+//    The type of existing Views
+    public static final int HEADER = 0;
+    public static final int LABEL_AND_VALUE = 1;
+
+    private int typeView;
+    private String value;
+    private String label;
+
+    public BaseStaticForm(int typeView) {
+        this.typeView = typeView;
+    }
+
+    public int getTypeView() {
+        return typeView;
+    }
+
+    public BaseStaticForm setTypeView(int typeView) {
+        this.typeView = typeView;
+        return this;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public BaseStaticForm setValue(String value) {
+        this.value = value;
+        return this;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public BaseStaticForm setLabel(String label) {
+        this.label = label;
+        return this;
+    }
 }
